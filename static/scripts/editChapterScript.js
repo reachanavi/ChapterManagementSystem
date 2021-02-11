e6253febe2763e117a4dac823cf07854bf769c73
reachanavi <reachanavi@gmail.com>
2021-02-11 12:08:14 -0600

    Did all the edit chapter related functions saveChanges() and cancel() functions Also added setters to Chapter class

--- static/scripts/editChapterScript.js
+++ static/scripts/editChapterScript.js
@@ -2,6 +2,10 @@
 //browser: http://localhost:8000
 // ctr C to end server
 
+//test:
+// document.getElementById("test-p").innerHTML = "Test text";
+
+
 //CLASSES:
 class TeamMember {
     constructor(name, email, idNumber, chaptersArr)
@@ -89,11 +93,45 @@ class Chapter {
         return this.teamMemberID;
     }
 
+    setLeaderName(name)
+    {
+        this.leaderName = name;
+    }
+
+    setLeaderEmail(email)
+    {
+        this.leaderEmail = email;
+    }
+
+    setSchool(school)
+    {
+        this.school = school;
+    }
+
+    setCity(city)
+    {
+        this.city = city;
+    }
+
+    setDescription(description)
+    {
+        this.description = description;
+    }
+
+    setSupport(support)
+    {
+        this.support = support;
+    }
+
     updateStatus(newStatus)
     {
         this.status = newStatus;
     }
-    
+
+    setChapterID(id)
+    {
+        this.chapterID = id;
+    }
 
     setTeamMember(id)
     {
@@ -178,5 +216,69 @@ function loadForm()
     document.getElementById("heading").style.color = "blue";
 }
 
+function saveChanges()
+{
+    //test:
+    document.getElementById("test-p").innerHTML = "Test text";
+
+    chapter.setLeaderName(document.getElementById("fname").value);
+    chapter.setLeaderEmail(document.getElementById("femail").value);
+    chapter.setSchool(document.getElementById("fschool").value);
+    chapter.setDescription(document.getElementById("fdescription").value);
+    chapter.setSupport(document.getElementById("fsupport").value);
+    chapter.updateStatus(parseInt(document.getElementById("fstatus").value));
+    chapter.updateStatus(parseInt(document.getElementById("fstatus").value));
+
+    let newMemberID = parseInt(document.getElementById("fteamMember").value);
+    if(newMemberID != chapter.getTeamMemberID())
+    {
+        //first, delete this chapter from the old member's list
+        let oldMember = teamMembers[chapter.getTeamMemberID() - 1];
+        for(i = 0; i < oldMember.getChapterList().length; i++)
+        {
+            if(oldMember.getChapterList()[i].getChapterID == chapter.getChapterID)
+            {
+                oldMember.getChapterList().splice(i, 1);
+            }
+        }
+
+        //update teamMember ID in the chapter object
+        chapter.setTeamMember(newMemberID);
+
+        //Add the chapter to the new team member's list
+        teamMembers[newMemberID - 1].addChapter(chapter);
+    }else{
+        let member = teamMembers[chapter.getTeamMemberID() - 1];
+        for(i = 0; i < member.getChapterList().length; i++)
+        {
+            if(member.getChapterList()[i].getChapterID() == chapter.getChapterID())
+            {
+                teamMembers[chapter.getTeamMemberID() - 1].getChapterList()[i] = chapter;
+            }
+        }
+
+    }
+
+    //test:
+    document.getElementById("test-p").innerHTML = "ran";
+    //Update local storage:
+    window.localStorage.setItem("team", JSON.stringify(teamMembers));
+
+    //Set everything back to null and go back to index
+    chapter = null;
+    window.sessionStorage.setItem("currentChapterEdit", "");
+    window.location.href = "index.html";
+
+}
+
+function cancel()
+{
+    loadForm();
+    document.getElementById("test-p").innerHTML = "cancel clicked";
+
+    chapter = null;
+    window.sessionStorage.setItem("currentChapterEdit", "");
+    window.location.href = "index.html";
+}
 
 
